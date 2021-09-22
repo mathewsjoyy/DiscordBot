392acc7492bd87f78f141f535f5b23d7778f4760
mathews <mathews2492@gmail.com>
2021-09-22 13:18:08 +0100

    added an eightball

--- src/main.py
+++ src/main.py
@@ -153,6 +153,37 @@ async def close_bot(ctx):
     await ctx.send(random.choice(replies))
     await bot.close()
     
+@bot.command(aliases=['8B', '8ball', '8Ball'])
+async def eightBall(ctx,*, question):
+    answers = [
+            "🟢 It is certain.",
+            "🟢 It is decidedly so.",
+            "🟢 Without a doubt.",
+            "🟢 Yes definitely.",
+            "🟢 You may rely on it.",
+
+            "🟢 As I see it, yes.",
+            "🟢 Most likely.",
+            "🟢 Outlook good.",
+            "🟢 Yes.",
+            "🟢 Signs point to yes.",
+
+            "🟠 Reply hazy, try again.",
+            "🟠 Ask again later.",
+            "🟠 Better not tell you now.",
+            "🟠 Cannot predict now.",
+            "🟠 Concentrate and ask again.",
+
+            "🔴 Don't count on it.",
+            "🔴 My reply is no.",
+            "🔴 My sources say no.",
+            "🔴 Outlook not so good.",
+            "🔴 Very doubtful."
+            ]
+
+    await ctx.reply(f"*Question :* **{question}***, Answer :* **{random.choice(answers)}**")
+    
+    
 
     
 if __name__ == "__main__":
